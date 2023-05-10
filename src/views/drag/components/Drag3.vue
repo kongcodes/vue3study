ae54e01bd5acf3feaa6201391a7b2a19a9eda439
kongcodes <78190504+kongcodes@users.noreply.github.com>
2023-05-11 00:03:50 +0800

    vue.draggable.next 使用

---
+++ src/views/drag/components/Drag3.vue
@@ -0,0 +1,73 @@
+<template>
+	<div style="margin-left: 100px;display: flex;margin-top: 300px;">
+		<draggable
+			group="group"
+			:list="state.list1"
+			:force-fallback="true"
+			chosen-class="chosen"
+			animation="300"
+			@start="onStart"
+			@end="onEnd"
+		>
+			<template #item="{ element }">
+				<div class="item bck1">
+					{{ element }}
+				</div>
+			</template>
+		</draggable>
+
+		<draggable
+			group="group"
+			:list="state.list2"
+			:force-fallback="true"
+			chosen-class="chosen"
+			animation="300"
+			@start="onStart"
+			@end="onEnd"
+		>
+			<template #item="{ element }">
+				<div class="item bck2">
+					{{ element }}
+				</div>
+			</template>
+		</draggable>
+	</div>
+</template>
+
+<script setup>
+import draggable from 'vuedraggable'
+import { reactive } from 'vue'
+
+const state = reactive({
+	list1: [1, 2, 3, 4],
+	list2: ['a', 'b', 'c', 'd'],
+})
+
+function onStart() {}
+
+function onEnd() {
+	console.log(state)
+}
+
+</script>
+
+<style scoped lang="scss">
+.item {
+	width: 200px;
+	line-height: 40px;
+	border-radius: 6px;
+	color: #fff;
+	margin-bottom: 10px;
+}
+.bck1 {
+	background: #ff7dc0 !important;
+	margin-left: 30px;
+}
+.bck2 {
+	background: #ccc !important;
+	margin-left: 30px;
+}
+.chosen {
+	background: #42b983 !important;
+}
+</style>
--- src/views/drag/index.vue
+++ src/views/drag/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="re">
+  <div style="margin-bottom: 300px;">
     <Drag1></Drag1>
     <Drag2></Drag2>
+    <Drag3></Drag3>
   </div>
 </template>
 
 <script setup>
 import Drag1 from './components/Drag1.vue';
 import Drag2 from './components/Drag2.vue';
+import Drag3 from './components/Drag3.vue';
 </script>
 
 <style lang="scss" scoped>
